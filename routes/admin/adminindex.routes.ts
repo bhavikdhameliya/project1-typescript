47e2a716cbac19b24cde6440a57593db91ffb85a
bhavik dhameliya <bhavikdhameliya20@gmail.com>
2024-01-23 17:25:35 +0530

    ex

--- routes/admin/adminindex.routes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import adminRoute from '../../routes/admin/admin.routes';
-
-const admin = express.Router();
-
-admin.use('/admin', adminRoute);
-
-export default admin
--- routes/user/userindex.routes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import userRoute from '../../routes/user/user.routes';
-
-const user = express.Router();
-
-user.use('/user', userRoute);
-
-
-export default user;
